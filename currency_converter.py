2abe9bb151d5880d4ede5d3b51468809abd6f0e6
EbiScott <egbejuleeworitse@gmail.com>
2023-12-22 23:12:00 +0100

    created the currency converter

---
+++ currency_converter.py
@@ -0,0 +1,19 @@
+#My Python script to convert currencies in realtime
+
+#Imports
+from forex_python.converter import CurrencyRates
+
+#Random object --- still useful though
+currency = CurrencyRates()
+
+#function
+def converter(amount, original_currency, final_currency):
+    global currency
+    
+
+#Needed variables
+
+
+
+#remember to inform users that they need to be connected to use my currency converter 
+#since it works in real-time
